48e06077010aa4e7fa7356fad22285d7edf697c6
dackae <rosalunaicastel@gmailcom>
2025-03-31 19:25:04 +0200

    pequeña rapida actualización a la base de datos

--- BD/creacionBD.sql
+++ BD/creacionBD.sql
@@ -3,9 +3,15 @@ create DATABASE IF NOT EXISTS CI_NA_Tickets;
 use CI_NA_Tickets;
 
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'PerroSanxeDimision';
+CREATE USER 'empleado'@'localhost' IDENTIFIED BY 'BartomeuDimision';
 CREATE USER 'client'@'localhost' IDENTIFIED BY '1234';
 
-GRANT ALL PRIVILEGES ON CI_NA_Tickets.Tickets TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON CI_NA_Tickets.* TO 'admin'@'localhost';
+GRANT SELECT, INSERT, DROP, UPDATE ON CI_NA_Tickets.Tickets TO 'empleado'@'localhost';
+GRANT SELECT, INSERT, DROP, UPDATE ON CI_NA_Tickets.Clientes TO 'empleado'@'localhost';
+GRANT SELECT, INSERT, DROP, UPDATE ON CI_NA_Tickets.Servicios TO 'empleado'@'localhost';
+GRANT SELECT, INSERT, DROP, UPDATE ON CI_NA_Tickets.Categoria TO 'empleado'@'localhost';
+GRANT SELECT, INSERT, DROP, UPDATE ON CI_NA_Tickets.Tickets TO 'client'@'localhost';
 
 create table IF NOT EXISTS Categoria(
     id_categoria INT AUTO_INCREMENT NOT NULL UNIQUE,
@@ -28,11 +34,11 @@ CREATE TABLE IF NOT EXISTS Usuarios (
     nombreApellido VARCHAR(100),
     email VARCHAR(100) UNIQUE NOT NULL,
     contrasena VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id_usuario),
+    PRIMARY KEY (id_usuario)
 );
 
 CREATE TABLE IF NOT EXISTS Empleados (
-    id_empleado INT AUTO_INCREMENT UNIQUE,
+    id_empleado INT AUTO_INCREMENT UNIQUE NOT NULL,
     id_usuario INT NOT NULL UNIQUE,
     id_categoria INT NOT NULL ,
     PRIMARY KEY (id_empleado),
@@ -40,7 +46,7 @@ CREATE TABLE IF NOT EXISTS Empleados (
 );
 
 CREATE TABLE IF NOT EXISTS Clientes (
-    id_cliente INT AUTO_INCREMENT UNIQUE,
+    id_cliente INT AUTO_INCREMENT UNIQUE NOT NULL,
     versionServicio INT,
     id_usuario INT NOT NULL UNIQUE,
     PRIMARY KEY (id_cliente),
@@ -49,7 +55,7 @@ CREATE TABLE IF NOT EXISTS Clientes (
 
 CREATE TABLE IF NOT EXISTS Tickets (
     id_ticket INT AUTO_INCREMENT PRIMARY KEY,
-    id_empleado INT NULL,
+    id_empleado INT NOT NULL,
     id_cliente INT NOT NULL,
     id_categoria INT NOT NULL,
     asunto VARCHAR(200) NOT NULL,
@@ -58,7 +64,7 @@ CREATE TABLE IF NOT EXISTS Tickets (
     estado VARCHAR(25) NOT NULL DEFAULT 'Abierto',
     fechaEmision DATE NOT NULL DEFAULT (CURRENT_DATE),
     fechaCierre DATE NULL,
-    FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado) ON DELETE SET NULL,
+    FOREIGN KEY (id_empleado) REFERENCES Empleados(id_empleado) ON DELETE CASCADE,
     FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente) ON DELETE CASCADE,
     FOREIGN KEY (id_categoria) REFERENCES Categoria(id_categoria) ON DELETE CASCADE
 );
