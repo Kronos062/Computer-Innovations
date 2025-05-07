543cf0a09cfff801ec2d35baf46cb4076c8f08af
Kronos062 <73135047+Kronos062@users.noreply.github.com>
2025-05-07 14:32:22 +0200

    actualizo el login
    he dejado el usuario simulado, me puesto la conexión a la BD para el user y corregido errores

--- login/php/login.php
+++ login/php/login.php
@@ -1,7 +1,6 @@
 <?php
 include 'conexion.php';
 session_start();
-
 header('Content-Type: application/json');
 
 if ($_SERVER["REQUEST_METHOD"] === "GET") {
@@ -25,27 +24,42 @@ if (!isset($datos['correo']) || !isset($datos['contraseña'])) {
 $correo = $datos['correo'];
 $contraseña = $datos['contraseña'];
 
-$correo_simulado = "ejemplo@gmail.com";
-$nombre_simulado = "cliente";
-$id_simulado = 5;
-$contraseña_simulada = "1234";
-if ($correo === $correo_simulado && $contraseña === $contraseña_simulada) {
+$sql = "SELECT id_usuario, nombreApellido, contrasena FROM Usuarios WHERE email = ?";
+$stmt = $conexion->prepare($sql);
+$stmt->bind_param("s", $correo);
+$stmt->execute();
+$stmt->store_result();
 
-    setcookie('usuario_nombre', $nombre_simulado, time() + 3600, '/');
-    setcookie('id_usuario', $id_simulado, time() + 3600, '/');
-    
-    if ($contraseña === $contraseña_simulada) {
-        $_SESSION['usuario_id'] = $id;
+if ($stmt->num_rows > 0) {
+    $stmt->bind_result($id_usuario, $nombre, $hash);
+    $stmt->fetch();
+
+    if (password_verify($contraseña, $hash)) {
+        setcookie('usuario_nombre', $nombre, time() + 3600, '/');
+        setcookie('id_usuario', $id_usuario, time() + 3600, '/');
+        $_SESSION['usuario_id'] = $id_usuario;
         $_SESSION['usuario_nombre'] = $nombre;
-
         echo json_encode(["mensaje" => "Has iniciado sesión correctamente"]);
     } else {
         http_response_code(403);
         echo json_encode(["error" => "Contraseña incorrecta"]);
     }
 } else {
-    http_response_code(404);
-    echo json_encode(["error" => "El usuario no existe"]);
+    $correo_simulado = "computerinnovations@gmail.com";
+    $nombre_simulado = "CI";
+    $id_simulado = 5;
+    $contraseña_simulada = "1234";
+
+    if ($correo === $correo_simulado && $contraseña === $contraseña_simulada) {
+        setcookie('usuario_nombre', $nombre_simulado, time() + 3600, '/');
+        setcookie('id_usuario', $id_simulado, time() + 3600, '/');
+        $_SESSION['usuario_id'] = $id_simulado;
+        $_SESSION['usuario_nombre'] = $nombre_simulado;
+        echo json_encode(["mensaje" => "Has iniciado sesión correctamente"]);
+    } else {
+        http_response_code(404);
+        echo json_encode(["error" => "El usuario no existe o contraseña incorrecta"]);
+    }
 }
 
 $stmt->close();
