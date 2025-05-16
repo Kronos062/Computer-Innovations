98e1c5999b92c9d3aecf1ea7e66985cd6320daf0
Kronos062 <cristiaan0662@gmail.com>
2025-05-16 22:01:50 +0200

    ARREGLO EL LOGOUT

--- login/php/logout.php
+++ login/php/logout.php
@@ -5,10 +5,10 @@ $_SESSION = [];
 session_destroy();
 
 if (isset($_COOKIE['usuario_nombre'])) {
-    setcookie('usuario_nombre', '', time() - 1, '/');
+    setcookie('usuario_nombre', '', time() - 3600, '/', '', false, true);
 }
 if (isset($_COOKIE['id_usuario'])) {
-    setcookie('id_usuario', '', time() - 1, '/');
+    setcookie('id_usuario', '', time() - 3600, '/', '', false, true);
 }
 echo json_encode(["res" => "Has cerrado sesión correctamente"]);
 ?>
